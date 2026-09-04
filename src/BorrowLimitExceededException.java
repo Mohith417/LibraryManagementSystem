23f550f1ef8eae0483d98eb479e19d28020ebc23
A MOHITH SANTOSH <amohith007@gmail.com>
2026-09-04 15:30:11 +0530

    Refactor

--- src/BorrowLimitExceededException.java
+++
@@ -1,8 +0,0 @@
-// Custom exception used when a member tries to borrow more than 3 books.
-public class BorrowLimitExceededException extends Exception {
-
-    // Constructor receives the error message.
-    public BorrowLimitExceededException(String message) {
-        super(message);
-    }
-}
