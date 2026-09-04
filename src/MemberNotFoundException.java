79bfa0eeab6e5f7fe9a55383e43df4b3f65cb33c
A MOHITH SANTOSH <amohith007@gmail.com>
2026-09-04 15:30:48 +0530

    Refactor

--- src/MemberNotFoundException.java
+++
@@ -1,8 +0,0 @@
-// Custom exception used when a member ID is not found.
-public class MemberNotFoundException extends Exception {
-
-    // Constructor receives the error message.
-    public MemberNotFoundException(String message) {
-        super(message);
-    }
-}
