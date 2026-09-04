3ff2ee33d0bcc4ebabb651c14cc2937cac1203b4
A MOHITH SANTOSH <amohith007@gmail.com>
2026-09-04 15:28:29 +0530

    Refactor

--- src/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-// Custom exception used when a book ID is not found.
-public class BookNotFoundException extends Exception {
-
-    // Constructor receives the error message.
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
